a6a4d41a65e274f05ab7ed900e263a637aae4cf7
Eduardo Bessa <eduardo.bessa@itpeople.pt>
2019-09-26 18:44:05 +0100

    Short Codes Upgrade

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 
+#Short Codes
+
+I share short codes I use every PHP projects. I try helping new PHP Developers.
+
+___
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-	require_once('./Database.php');
-	
-	
-	$database = new MySQL();
-	$database->table("tbl_accounts")->select()->where("account_user", "LIKE", "eduubessa")->order('teste', 'ASC')->limit(5)->get();
